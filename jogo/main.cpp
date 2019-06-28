229587f796a418b26fc955685b4cf622e9d36628
henriqueherrera <henriqueherrera9@gmail.com>
2019-06-28 15:22:54 -0300

    qualquer coisa

--- jogo/main.cpp
+++ jogo/main.cpp
@@ -8,8 +8,8 @@
 
 using namespace std;
 
-SDL_Surface *screen, *imgFly, *imgBackGroung[2], *text, *imgPerson = NULL;
-SDL_Rect movBackGround[3], movGround[2],rectFly[4], movFly;
+SDL_Surface *screen, *imgFly, *imgBackGroung[2], *text, *placar, *imgPerson = NULL;
+SDL_Rect movBackGround[3], movGround[2],rectFly[4], movFly, rectPonto;
 SDL_Rect rectBackGroung[2], rectGround, rectPerson[3], movPerson;
 
 Uint32 color;
@@ -18,23 +18,25 @@ Uint32 color2;
 int mov_ground[3] = {0,620,1220};
 int fly = 810;
 int animaPerson = 0;
+int numberSpriteFly, numberSpritePerson;
 int animaFly = 0;
 bool running = true;
+bool gameOver = false;
 
-void delayBlitPerson(){
-	while( running ){
+void delayBlitPerson() {
+	while( running ) {
     	this_thread::sleep_for(chrono::milliseconds(150));
 		animaPerson++;
 		animaFly++;
-		if( animaPerson == 3){
+		if( animaPerson == 3 ) {
 			animaPerson = 0;
 		}
-		if(animaFly == 4){
+		if( animaFly == 4 ) {
 			animaFly = 0;
 		}
 	}
 }
-void setFly(){
+void setFly() {
 	imgFly = IMG_Load("./fly.png");
 	rectFly[0].x =6;
 	rectFly[0].y =73;
@@ -55,12 +57,13 @@ void setFly(){
 	movFly.x = 810;
 	movFly.y = 50;
 }
-void setBackGroung(){
+void setBackGroung() {
 	int movBg[3] = {0, 1280, 2520};
-    imgBackGroung[0] = SDL_DisplayFormat(SDL_LoadBMP("fundo.bmp"));
-    imgBackGroung[1] = SDL_DisplayFormat(SDL_LoadBMP("fundo2.bmp"));
-    for(int i = 1; i<= 3; i++){
-        rectBackGroung[i-1].w = 1280;
+	imgBackGroung[0] = SDL_DisplayFormat(SDL_LoadBMP("fundo.bmp"));
+  imgBackGroung[1] = SDL_DisplayFormat(SDL_LoadBMP("fundo2.bmp"));
+    
+	for(int i = 1; i<= 3; i++){
+		rectBackGroung[i-1].w = 1280;
 		rectBackGroung[i-1].h = 620;
         rectBackGroung[i-1].x = 0;
         rectBackGroung[i-1].y = 0;
@@ -74,7 +77,7 @@ void setBackGroung(){
 	movGround[1].x = 1240;
 	movGround[1].y = 0;
 }
-void setPerson(){
+void setPerson() {
 	imgPerson = IMG_Load("./drag.png");
 	rectPerson[0].x  = 8;
 	rectPerson[0].y  = 134;
@@ -92,23 +95,33 @@ void setPerson(){
 	movPerson.y = 100;
 }
 
-void blitScreen(){
-    SDL_FillRect(screen, &screen->clip_rect, color2);
-    SDL_BlitSurface(imgBackGroung[0],&rectBackGroung[0], screen, &movBackGround[0]);
-    SDL_BlitSurface(imgBackGroung[1],&rectBackGroung[0], screen, &movBackGround[1]);
+void blitScreen() {
+  SDL_FillRect(screen, &screen->clip_rect, color2);
+  SDL_BlitSurface(imgBackGroung[0],&rectBackGroung[0], screen, &movBackGround[0]);
+	SDL_BlitSurface(imgBackGroung[1],&rectBackGroung[0], screen, &movBackGround[1]);
 	SDL_BlitSurface(imgFly,&rectFly[animaFly], screen, &movFly);
-    SDL_BlitSurface(text,NULL, screen, NULL);
-    SDL_BlitSurface(imgPerson,&rectPerson[animaPerson], screen, &movPerson);
-    SDL_Flip(screen);
+  SDL_BlitSurface(imgPerson,&rectPerson[animaPerson], screen, &movPerson); 
+  
+	SDL_BlitSurface(placar, NULL, screen, &rectPonto);
+	SDL_Flip(screen);
 }
 
-void setGame(){
+string gameOverTxt(int score) {
+	rectPonto.x = 200;
+	rectPonto.y = 200;
+	string x = "Game Over\nPontos:	"+to_string(score)+"\nPara recomeçar aperte(Enter)\nPara Sair do jogo aperte(Espaço)";
+	return x;
+}
+void setGame() {
 	setPerson();
-    setBackGroung();
-    setFly();
+  setBackGroung();
+  setFly();
+	rectPonto.x = 300;
+	rectPonto.y = 0;
+	
 }
 
-void mov(){
+void mov() {
 	movGround[0].x--;
 	movGround[1].x--;
 	movBackGround[0] = movGround[0];
@@ -120,23 +133,24 @@ void mov(){
 		movGround[1].x = 1240;
 	}
 }
-int main (int argc, char ** argcv){
+int main (int argc, char ** argcv) {
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-    TTF_Init();
-    TTF_Font *font = TTF_OpenFont("air.TTF",32);
-    screen = SDL_SetVideoMode(800,600,32,SDL_SWSURFACE);
-    const int FPS = 60;
-    Uint32 start; //int de 32bits
-    color = SDL_MapRGB(screen->format, 0xff, 0xff, 0xff);
-    color2 = SDL_MapRGB(screen->format, 0x00, 0x00, 0x00);
+  SDL_Init(SDL_INIT_EVERYTHING);
+  TTF_Init();
+  TTF_Font *font = TTF_OpenFont("air.TTF",32);
+  screen = SDL_SetVideoMode(800,600,32,SDL_SWSURFACE);
+  const int FPS = 60;
+  Uint32 start; //int de 32bits
+  color = SDL_MapRGB(screen->format, 0xff, 0xff, 0xff);
+  color2 = SDL_MapRGB(screen->format, 0x00, 0x00, 0x00);
 	thread  delay(delayBlitPerson);
-    bool right = false;
-	bool gameOver = false;
+  bool right = false;
 	int sort = 1;
-    SDL_Color color3 = {0,0,0};
-    bool leftPressed = false, rightPressed = false, saltPressed = false; 
-    setGame();
+	int score =0;
+	string pontos = "Pontos: 0";
+  placar = TTF_RenderText_Solid(font, pontos.c_str(), {210,40,89});
+  bool leftPressed = false, rightPressed = false, saltPressed = false; 
+  setGame();
 	while(running){
         start = SDL_GetTicks();
         
@@ -170,67 +184,48 @@ int main (int argc, char ** argcv){
 					break;
 	   		}	     
 		}
-		fly-=sort;
-		movFly.x = fly;
-		if (fly <= -58)
-		{
-			fly = 810;
-			movFly.y = rand()%500;
-			sort = rand()%9+1;
-		}
-		if( movFly.y> movPerson.y){
-			movFly.y--;
-		}else{
-			movFly.y++;
-		}		
-		if(saltPressed){
-			movPerson.y-=5;
-		}
-		movPerson.y++;
-		if(movPerson.y > 500){
-			text = TTF_RenderText_Solid(font,"Game Over",{210,40,89});
-		}else{
-			text = TTF_RenderText_Solid(font,"Game Over",{210,40,89});			
-		}
-		if((movPerson.y >= movFly.y && movPerson.y <= movFly.y+56) && (movPerson.x+112>= movFly.x  && movPerson.x+112<= movFly.x+58))
-		{
-			text = TTF_RenderText_Solid(font,"Game Over",{210,40,89});
+		
+		if((movPerson.y >= movFly.y && movPerson.y <= movFly.y+56) && (movPerson.x+112>= movFly.x  && movPerson.x+112<= movFly.x+58)) {
 			gameOver = true;
 		}else
-		{
-			text = TTF_RenderText_Solid(font,"Funciona",{210,40,89});
-		}
-		
-		if(gameOver){
-			delay.join();
-			while (gameOver){
-				blitScreen();
-				cout<< "aaaaaaaaaaaaaaaaaaaaaaaaaaa"<<endl;
-				switch (event.type)
-				{
-				case SDL_KEYDOWN:
-					if(event.key.keysym.sym == SDLK_SPACE){
-						gameOver = false;
-					}
-				
-				default:
-					break;
-				}
-			}
-			thread  delay(delayBlitPerson);
-		}
-		
 		blitScreen();
 		mov();
 		if( 500/FPS > SDL_GetTicks()-start) {
 			SDL_Delay(500/FPS-(SDL_GetTicks()-start));
 		}
-		
-    }
-    SDL_FreeSurface(text);
-    SDL_FreeSurface(imgBackGroung[0]);
-    TTF_CloseFont(font);
-    SDL_Quit();
-    delay.join(); 
-    return 0;
+		if(!gameOver) {
+
+			fly-=sort;
+			movFly.x = fly;
+			if (fly <= -58) {
+				score++;
+				fly = 810;
+				movFly.y = rand()%500;
+				sort = rand()%9+1;
+				pontos = "Pontos: "+to_string(score);
+				cout<<pontos<<endl;
+				placar = TTF_RenderText_Solid(font, pontos.c_str(), {210,40,89});
+			}
+			if( movFly.y> movPerson.y){
+				movFly.y--;
+			}else{
+				movFly.y++;
+			}		
+			if(saltPressed){
+				movPerson.y-=5;
+			}
+			movPerson.y++;
+				
+		}else{
+			cout<<gameOverTxt(score)<<endl;
+			placar = TTF_RenderText_Solid(font,"me mata",{210,40,89});
+		}
+
+	} 
+  SDL_FreeSurface(text);
+  SDL_FreeSurface(imgBackGroung[0]);
+  TTF_CloseFont(font);
+  SDL_Quit();
+  delay.join(); 
+  return 0;
 }
